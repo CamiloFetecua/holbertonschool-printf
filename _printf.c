0492f42c7426e73396d28439dc9f92ba3e118f6c
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-04 13:13:14 -0800

    Added char, string and function calls, also defined the cases for each function

--- _printf.c
+++ _printf.c
@@ -3,17 +3,30 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
-	va_list arg_pf;
+	va_list arguments;
 	const char *pf;
-	va_start(arg_pf, format);
+	va_start(arguments, format);
 
 	for(pf = format; *pf != '\0'; pf++)
 	{
 		if (*pf == '%' && *(pf + 1) != '\0')
 		{
-		
+			pf++;
+			switch (*pf)
+			{
+				case 'c'
+					count = printChar(va_list arguments);
+					break;
+				case 's'
+					count = printString(va_list arguments);
+					break;
+				case '%'
+					count = printString(va_list arguments);
+                                        break;
+			}
 		}
 	}
 
 	va_end(arg_pf);
+	return (count);
 }
