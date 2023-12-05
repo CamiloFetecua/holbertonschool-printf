b00724ca2c8082a8f76d92e2fa61ff785eb54acd
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-05 06:55:34 -0800

    add test on main.c and compare printf and _printf functions

--- main.c
+++ main.c
@@ -1,23 +1,38 @@
 #include "main.h"
 #include <limits.h>
+#include <stdio.h>
 
 int main(void)
 {
     int len;
     unsigned int ui;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d]\n", len);
+    len = _printf("Vamos a intentar imprimir una oración simple.\n");
+    _printf("Longitud:[%d]\n", len);
 
-    len = _printf("Character:[%c]\n", 'H');
-    _printf("Length:[%d]\n", len);
+    len = _printf("Carácter:[%c]\n", 'H');
+    _printf("Longitud:[%d]\n", len);
 
-    len = _printf("String:[%s]\n", "I am a string !");
-    _printf("Length:[%d]\n", len);
+    len = _printf("Cadena:[%s]\n", "Soy una cadena !");
+    _printf("Longitud:[%d]\n", len);
 
     ui = (unsigned int)INT_MAX + 1024;
-    len = _printf("Unsigned:[%u]\n", ui);
-    _printf("Length:[%d]\n", len);
+    len = _printf("Sin signo:[%u]\n", ui);
+    _printf("Longitud:[%d]\n", len);
+
+    len = printf("Vamos a intentar imprimir una oración simple.\n");
+    printf("Longitud:[%d]\n", len);
+
+    len = printf("Carácter:[%c]\n", 'H');
+    printf("Longitud:[%d]\n", len);
+
+    len = printf("Cadena:[%s]\n", "Soy una cadena !");
+    printf("Longitud:[%d]\n", len);
+
+    ui = (unsigned int)INT_MAX + 1024;
+    len = printf("Sin signo:[%u]\n", ui);
+    printf("Longitud:[%d]\n", len);
+
 
     return (0);
 }
